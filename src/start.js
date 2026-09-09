244d04cc23fbb3ef7993ab14e7bbe388771e3d3f
Alansillo78 <alantorre05@gmail.com>
2026-09-09 16:40:30 -0600

    Set up Bots role on startup

--- src/start.js
+++ src/start.js
@@ -2,7 +2,9 @@ import './index.js';
 import { startTikTokDetector } from './tiktok-detector.js';
 import { startAntiSpam } from './anti-spam.js';
 import { startVRMonitor } from './vr-monitor.js';
+import { setupBotRole } from './bot-role.js';
 
 startTikTokDetector();
 startAntiSpam();
 startVRMonitor();
+setTimeout(() => setupBotRole(), 10000);
