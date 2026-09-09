95650571baa61efb6601a69dddd57c78f0801c94
Alansillo78 <alantorre05@gmail.com>
2026-09-09 15:40:33 -0600

    Restore VR channels and set up sponsor section

--- src/start.js
+++ src/start.js
@@ -3,8 +3,10 @@ import { startTikTokDetector } from './tiktok-detector.js';
 import { startAntiSpam } from './anti-spam.js';
 import { publishRules } from './rules.js';
 import { startVRMonitor } from './vr-monitor.js';
+import { setupSponsorChannels } from './sponsor-channels.js';
 
 startTikTokDetector();
 startAntiSpam();
 startVRMonitor();
 setTimeout(() => publishRules(), 5000);
+setTimeout(() => setupSponsorChannels(), 8000);
