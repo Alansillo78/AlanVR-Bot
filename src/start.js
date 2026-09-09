9a1c9c4dff562590f88ffab34c200331af93265c
Alansillo78 <alantorre05@gmail.com>
2026-09-09 15:31:21 -0600

    Start VR news and deals monitor

--- src/start.js
+++ src/start.js
@@ -2,7 +2,9 @@ import './index.js';
 import { startTikTokDetector } from './tiktok-detector.js';
 import { startAntiSpam } from './anti-spam.js';
 import { publishRules } from './rules.js';
+import { startVRMonitor } from './vr-monitor.js';
 
 startTikTokDetector();
 startAntiSpam();
+startVRMonitor();
 setTimeout(() => publishRules(), 5000);
