8cc21edab5957ad15a16f32ab361a1d252b3e66a
Alansillo78 <alantorre05@gmail.com>
2026-09-09 15:21:40 -0600

    Publish server rules on startup

--- src/start.js
+++ src/start.js
@@ -1,6 +1,8 @@
 import './index.js';
 import { startTikTokDetector } from './tiktok-detector.js';
 import { startAntiSpam } from './anti-spam.js';
+import { publishRules } from './rules.js';
 
 startTikTokDetector();
 startAntiSpam();
+setTimeout(() => publishRules(), 5000);
