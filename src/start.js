2d57115015ea3c8436cb2c468b8a09ba0a5cb1ba
Alansillo78 <alantorre05@gmail.com>
2026-09-09 16:30:46 -0600

    Stop republishing rules and sponsor showcase on restart

--- src/start.js
+++ src/start.js
@@ -1,12 +1,8 @@
 import './index.js';
 import { startTikTokDetector } from './tiktok-detector.js';
 import { startAntiSpam } from './anti-spam.js';
-import { publishRules } from './rules.js';
 import { startVRMonitor } from './vr-monitor.js';
-import { setupSponsorChannels } from './sponsor-channels.js';
 
 startTikTokDetector();
 startAntiSpam();
 startVRMonitor();
-setTimeout(() => publishRules(), 5000);
-setTimeout(() => setupSponsorChannels(), 8000);
